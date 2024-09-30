d6ca2f06025f4adae0551483bef20c40a736eb28
Antoine Peral <peral.antoine@gmail.com>
2024-09-30 16:02:34 +0200

    move articles data to folder data

---
+++ src/app/data/dataArticles.ts
@@ -0,0 +1,47 @@
+export const articles = [
+  { 
+    id: 1,
+    title: 'Angular 16: Les nouveautés', 
+    author: 'Alice', 
+    content: 'Les nouveautés d\'Angular 16 incluent...', 
+    image: 'https://via.placeholder.com/350x150',
+    imageTitle: 'firstImg',
+    isPublished: true, 
+    comment: '', 
+    likes: 120 
+  },
+  { 
+    id: 2,
+    title: 'Développer une API REST', 
+    author: 'Bob', 
+    content: 'Développer une API REST nécessite...', 
+    image: 'https://via.placeholder.com/350x150',
+    imageTitle: 'secondImg',
+    isPublished: false, 
+    comment: '', 
+    likes: 75 
+  },
+  { 
+    id: 3,
+    title: 'Pourquoi TypeScript est essentiel ?', 
+    author: 'Charlie', 
+    content: 'TypeScript apporte de la robustesse...', 
+    image: 'https://via.placeholder.com/350x150',
+    imageTitle: 'thirdImg',
+    isPublished: true, 
+    comment: '', 
+    likes: 200 
+  },
+  { 
+    id: 4,
+    title: 'Pourquoi manger est essentiel ?', 
+    author: 'Charlie', 
+    content: 'TypeScript apporte de la robustesse...', 
+    image: 'https://via.placeholder.com/350x150',
+    imageTitle: 'thirdImg',
+    isPublished: true, 
+    comment: '', 
+    likes: 50 
+  }
+];
+
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import {articles} from '../data/dataArticles'
 
 @Component({
   selector: 'app-home-page',
@@ -16,52 +17,7 @@ export class HomePageComponent {
   popularArticleColor = 'green';
   router: Router = inject(Router);
 
-  articles = [
-    { 
-      id: 1,
-      title: 'Angular 16: Les nouveautés', 
-      author: 'Alice', 
-      content: 'Les nouveautés d\'Angular 16 incluent...', 
-      image: 'https://via.placeholder.com/350x150',
-      imageTitle: 'firstImg',
-      isPublished: true, 
-      comment: '', 
-      likes: 120 
-    },
-    { 
-      id: 2,
-      title: 'Développer une API REST', 
-      author: 'Bob', 
-      content: 'Développer une API REST nécessite...', 
-      image: 'https://via.placeholder.com/350x150',
-      imageTitle: 'secondImg',
-      isPublished: false, 
-      comment: '', 
-      likes: 75 
-    },
-    { 
-      id: 3,
-      title: 'Pourquoi TypeScript est essentiel ?', 
-      author: 'Charlie', 
-      content: 'TypeScript apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/350x150',
-      imageTitle: 'thirdImg',
-      isPublished: true, 
-      comment: '', 
-      likes: 200 
-    },
-    { 
-      id: 4,
-      title: 'Pourquoi manger est essentiel ?', 
-      author: 'Charlie', 
-      content: 'TypeScript apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/350x150',
-      imageTitle: 'thirdImg',
-      isPublished: true, 
-      comment: '', 
-      likes: 50 
-    }
-  ];
+  articles : Article[] = articles;
 
   isAnyArticlePublished = this.articles.some((article)=> article.isPublished === true);
 
